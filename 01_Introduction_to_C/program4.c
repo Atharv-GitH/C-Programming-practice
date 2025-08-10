c71ac52766cbd9035790f4cb3cd524dd1093cf69
Atharv-GitH <atharv.kulkarni0709@gmail.com>
2025-08-11 01:54:55 +0530

    Add files via upload

---
+++ 01_Introduction_to_C/program4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Computer Engineering\n");
+
+	return 0;
+
+}
---
+++ 01_Introduction_to_C/program5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+
+int x;
+
+printf("Enter any number\n");
+scanf("%d",&x);
+printf("%d\n",x);
+}
